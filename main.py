1099ef64132c172599ee3677a4000fea65c34f40
vasiliyvn <mishadasha1114@gmail.com>
2024-07-17 18:41:55 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+print("Hello world")# This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -14,9 +14,21 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-name='pascal'
-print(name[0])
-print(name[-1])
-print(name[3:])
-print(name[::-1])
-print(name[1::2])
+print("1st program")
+print(9**0.5*5)
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1001)
+print("3nd program")
+print(type(1234 and 5678))
+print(23+67)
+print("4nd program")
+print(type(13.42 and 42.13))
+print((int(13.42*100//100))==(int(42.13*100%100)))
+
+
+
+
+
+
+
+
